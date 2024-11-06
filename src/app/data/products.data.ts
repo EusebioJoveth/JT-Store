20b27fa620b09fe014722baa82e4bf8d26275871
Eusebio Joveth <eusebio.joveth@porlande.com>
2024-11-06 19:01:27 +0100

    chore:update

--- src/app/data/products.data.ts
+++ src/app/data/products.data.ts
@@ -4,7 +4,7 @@ import { Products } from "../models/product";
 export const PRODUCTS: Products[] = [
   {
     id: '8347836c-35db-4307-ac5d-cbdf19a9a50c',
-    name: 'Black+Decker Helix Performance Premium Hand Mixer, 5-Speed Mixer, Red, MX600R',
+    name: 'Black+Decker Helix Performance Premium Hand Mixer, 5 velocidades, vermelho, MX600R',
     description: '',
     price: 102,
     thumbUrl:
@@ -13,7 +13,7 @@ export const PRODUCTS: Products[] = [
   },
   {
     id: 'f8c2cc34-ddb7-46ec-96ee-e27d1765df10',
-    name: 'KitchenAid Pro Line 5 Speed Hand Mixer, Aqua Sky',
+    name: 'KitchenAid Pro Line 5 Speed ​​Batedeira de Mão, Aqua Sky',
     description: '',
     price: 115,
     thumbUrl:
@@ -22,7 +22,7 @@ export const PRODUCTS: Products[] = [
   },
   {
     id: '2927b969-1c97-4a36-b4ab-a0777d8690e2',
-    name: 'KitchenAid 5-Speed Ultra Power Hand Mixer',
+    name: 'Batedeira manual ultra potente de 5 velocidades KitchenAid',
     description: '',
     price: 114,
     thumbUrl:
@@ -31,7 +31,7 @@ export const PRODUCTS: Products[] = [
   },
   {
     id: '57ba7441-8bea-4340-9f49-dc369d108cf7',
-    name: 'KitchenAid 5-Speed Hand Mixer',
+    name: 'Batedeira de mão KitchenAid de 5 velocidades',
     description: '',
     price: 105,
     thumbUrl:
@@ -40,7 +40,7 @@ export const PRODUCTS: Products[] = [
   },
   {
     id: '552809ab-7ea3-4e48-be23-3c7c53219284',
-    name: 'Cuisinart Power 5-Speed 220-Watt Hand Mixer, White',
+    name: 'Batedeira manual Cuisinart Power de 5 velocidades e 220 watts, branca',
     description: '',
     price: 119,
     thumbUrl:
@@ -50,7 +50,7 @@ export const PRODUCTS: Products[] = [
 
   {
     id: '893deaf7-5224-4702-9ba6-4fc4c52bed22',
-    name: 'KitchenAid 5 Speed Ultra Power Hand Mixer',
+    name: 'Batedeira de mão ultra potente KitchenAid de 5 velocidades',
     description: '',
     price: 114,
     thumbUrl:
@@ -60,7 +60,7 @@ export const PRODUCTS: Products[] = [
 
   {
     id: '26917fd7-61c3-41d7-b7cd-3c823bae77d2',
-    name: 'KitchenAid 5-Speed Hand Mixer, Empire Red',
+    name: 'Batedeira de mão KitchenAid de 5 velocidades, vermelho império',
     description: '',
     price: 120,
     thumbUrl:
@@ -69,7 +69,7 @@ export const PRODUCTS: Products[] = [
   },
   {
     id: '30e575f5-8997-4c34-aa44-88af74452db2',
-    name: 'Cuisinart 5-Speed Hand Mixer, Black',
+    name: 'Batedeira manual Cuisinart de 5 velocidades, preta',
     description: '',
     price: 110,
     thumbUrl:
